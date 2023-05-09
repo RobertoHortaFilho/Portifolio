ef0d5ce372af7dbdadeabcfe1eb533f2251addf1
RobertoHortaFilho <robertohortafilho@gmail.com>
2023-05-09 00:18:19 -0300

    feat: adicionando header

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import styles from './header.module.css'
+import Image from 'next/image'
+
+const Header = () => {
+    return(
+        <div className={styles.header}>
+            <Image
+                src="/images/perfil-picture.png"
+                alt="picture-profile"
+                className={styles.circleImage}
+                width={40}
+                height={40}
+                priority
+            />
+            <h1 className={styles.name}>Roberto Alessandro horta filho</h1>
+            <div className={styles.darkContainer}>
+                <Image
+                    src="/icons/dark.png"
+                    alt="dark-mode"
+                    className={styles.imageDarkmode}
+                    width={20}
+                    height={20}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default Header
