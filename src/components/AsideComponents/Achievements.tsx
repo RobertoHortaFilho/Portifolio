f29ae64fc67ad24feb9fa3717e4cbd7794b7d0bc
RobertoHortaFilho <robertohortafilho@gmail.com>
2023-05-10 01:30:01 -0300

    feat: adicionando descricao das badges 'mouse hover'

--- src/components/AsideComponents/Achievements.tsx
+++ src/components/AsideComponents/Achievements.tsx
@@ -3,29 +3,36 @@ import './achievements.css'
 import BadgeItem from './BadgeItem'
 
 const Achivements = () => {
-    return (
-        <div className='containerA'>
-            <div className='titleContainer'>
-                <div className='badgesContainer'>
-                    <Image 
-                        src='/icons/badge2.png'
-                        alt='badge'
-                        width={24}
-                        height={24}
-                        className='badge'
-                    />    
-                </div>
-                <p className='title'>Conquistas</p>
-            </div>
-            <div className='badges-Container'>
-                <BadgeItem />
-                <BadgeItem />
-                <BadgeItem />
-                <BadgeItem />
-                <BadgeItem />
-            </div>
-        </div>
-    )
+	return (
+		<div className='containerA'>
+			<div className='titleContainer'>
+				<div className='badgesContainer'>
+					<Image
+						src='/icons/badge2.png'
+						alt='badge'
+						width={24}
+						height={24}
+						className='badge'
+					/>
+				</div>
+				<p className='title'>Conquistas</p>
+			</div>
+			<div className='badges-Container'>
+				<BadgeItem>
+					<p className="popUp-title">Titulo</p>
+					<p className="popUp-desc">descricao sobre o que esse badge siguinifica e como foi "conquistada"</p>
+				</BadgeItem>
+				<BadgeItem>
+					<p className="popUp-title">Titulo</p>
+					<p className="popUp-desc">descricao sobre o que esse badge siguinifica e como foi "conquistada"</p>
+				</BadgeItem>
+				<BadgeItem>
+					<p className="popUp-title">Titulo</p>
+					<p className="popUp-desc">descricao sobre o que esse badge siguinifica e como foi "conquistada"</p>
+				</BadgeItem>
+			</div>
+		</div>
+	)
 }
 
 export default Achivements
--- src/components/AsideComponents/BadgeItem.tsx
+++ src/components/AsideComponents/BadgeItem.tsx
@@ -1,18 +1,36 @@
+"use client"
 import Image from "next/image";
 import './badgeItem.css'
+import { ReactElement, useState } from 'react'
 
-const BadgeItem = () => {
-    return (
-        <div className="containerBadge">
-            <Image
-                className="ImageBadge"
-                src='/images/Icon-anime.png'
-                alt='Medalha'
-                width={250}
-                height={250}
-            />
+interface IProps {
+  children: ReactElement[];
+}
+
+const BadgeItem = ({children} :IProps) => {
+  const [description, setDescription] = useState(false)
+  return (
+    <div
+      className="containerBadge"
+      onMouseEnter={() => setDescription(true)}
+      onMouseLeave={() => setDescription(false)}
+    >
+      {description && (
+        <div className="popUp-badge">
+          {children}
+          {/* <p className="popUp-title">Titulo</p>
+          <p className="popUp-desc">descricao sobre o que esse badge sighunifica e como foi "conquistada"</p> */}
         </div>
-    )
+      )}
+      <Image
+        className="ImageBadge"
+        src='/images/Icon-anime.png'
+        alt='Medalha'
+        width={250}
+        height={250}
+      />
+    </div>
+  )
 }
 
 export default BadgeItem
