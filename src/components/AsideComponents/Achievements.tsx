4e9cca2e71b8398b00dbc3b5f5d7f55e39f08adb
RobertoHortaFilho <robertohortafilho@gmail.com>
2023-05-10 00:11:36 -0300

    chore: adicionando imagem

--- src/components/AsideComponents/Achievements.tsx
+++ src/components/AsideComponents/Achievements.tsx
@@ -1,9 +1,26 @@
+import Image from 'next/image'
 import './achievements.css'
+import BadgeItem from './BadgeItem'
 
 const Achivements = () => {
     return (
-        <div className='container'>
-            <p className='title'>Conquistas</p>
+        <div className='containerA'>
+            <p className='titleContainer'>
+                <div className='badgesContainer'>
+                    <Image 
+                        src='/icons/badge2.png'
+                        alt='badge'
+                        width={24}
+                        height={24}
+                        className='badge'
+                    />    
+                </div>
+                <p className='title'>Conquistas</p>
+            </p>
+            <div className='badges-Container'>
+                <BadgeItem />
+                <BadgeItem />
+            </div>
         </div>
     )
 }
