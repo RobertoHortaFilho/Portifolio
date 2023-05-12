99f767b6d38051fd27fd33e5809677fe2242c172
RobertoHortaFilho <robertohortafilho@gmail.com>
2023-05-12 00:22:14 -0300

    feat: adicionando texto "sobre mim"

--- src/components/MainComponents/Sobre.tsx
+++ src/components/MainComponents/Sobre.tsx
@@ -3,7 +3,33 @@ import './sobre.css'
 const Sobre = () => {
   return (
     <div>
-      Sobre
+      <p>
+        Olá seja muitissimo bem vindo! eu sou o Roberto.
+      </p>
+      <p>
+        Minha história com a tecnologia vem de muito tempo atrás quando eu ganhei meu primeiro computador
+        em 2004, e eu tinha apenas 5 anos, minha família tinha muito medo de estragar, então eu juntei toda
+        a minha curiosidade para desbravar esse novo mundo, e eu tentava de tudo naquele PC!
+      </p>
+      <p>
+        Pouco tempo depois ganhei meu primeiro videogame, um playstation 2, e não demorou muito para me
+        apaixonar por jogos e como aquilo era feito, mas além de muito novo a internet ainda estava engatinhando
+        quando o assunto era programação de jogos independentes, por longos anos eu tentei programar modificações
+        para jogos, mas sempre sem muito sucesso.
+      </p>
+      <p>
+        Um bom tempo se passou e assim que acabei minha formação no ensino médio fui logo procurar algum
+        modo de continuar com meu sonho e acabei encontrando formação da Estácio em jogos digitais,
+        estudei a formaçao, mas ainda não era o que eu queria e depois de um tempo dei uma repaginada,
+        fui atras de uma formação para desenvolvimento web e encontrei a Trybe onde me especializei
+        em front e back, alem de um acompanhamento pessoas para me desenvolver como um profissional.
+      </p>
+      <p>
+      Atualmente tento sempre contribuir para o conhecimento de todos a minha volta sempre que posso
+        quando o assunto é tecnologia/programação eu converso por horas. Minha especialização em criação
+        de sites de todos os tipos com back-ends sólidos e front-ends responsivos para ser acessado de qualquer
+        lugar.
+      </p>
     </div>
   )
 }
