9ece646fb8ba1ae6d7fbcdcf38be0205a707f2e4
RobertoHortaFilho <robertohortafilho@gmail.com>
2023-05-31 15:47:18 -0300

    fix: erro com emojis

--- src/components/MainComponents/Hobbys.tsx
+++ src/components/MainComponents/Hobbys.tsx
@@ -1,16 +1,21 @@
 import './hobbys.css'
 
+const myHobbys = [
+  '🎮 Sou apaixonado por todos os tipos de jogos e suas possibilidades, dos cardgames aos de computador.',
+  '📖 Gosto de ler livros no tempo livre, técnicos, de psicologia e marketing.',
+  '🕹 Progamo jogos para aprender temas que estou estudando.',
+  '🎭 Entusiasta de arte, gosto de fazer muita "bagunça" e criar coisas novas.',
+  '👒 Nas horas vagas tento colocar em dia os animes.',
+  '🎸 Gosto de curti as musicas tocando elas com minhas proprias mãos, entao tenho um ukulele e uma guitarra',
+  '🧊 Tenho uma coleçao de cubos magicos e testes logicos',
+]
 
 const Hobbys = () => {
   return (
     <div className='hobbys'>
-      <p>🎮 Sou apaixonado por todos os tipos de jogos e suas possibilidades, dos cardgames aos de computador.</p>
-      <p>📖 Gosto de ler livros no tempo livre, técnicos, de psicologia e marketing.</p>
-      <p>🕹 Progamo jogos para aprender temas que estou estudando.</p>
-      <p>🎭 Entusiasta de arte, gosto de fazer muita 'bagunça' e criar coisas novas.</p>
-      <p>👒 Nas horas vagas tento colocar em dia os animes.</p>
-      <p>🎸 Gosto de curti as musicas tocando elas com minhas proprias mãos, entao tenho um ukulele e uma guitarra</p>
-      <p>🧊 Tenho uma coleçao de cubos magicos e testes logicos</p>
+      {myHobbys.map((i, k) => (
+        <p key={k}>{i}</p>
+      ))}
     </div>
   )
 }
