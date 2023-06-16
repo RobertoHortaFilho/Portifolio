2162e324543ce1c334fd306f945e9490be1c843c
RobertoHortaFilho <robertohortafilho@gmail.com>
2023-06-16 18:12:31 -0300

    feat: implementando contexto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,19 @@ import styles from './page.module.css'
 import Header from '@/components/Header'
 import Main from '@/components/Main'
 import Aside from '@/components/Aside'
+import GlobalContext from './GlobalContext'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Header />
-      <div className={styles.separete}/>
-      <div className={styles.corpo}>
-        <Aside />
-        <Main />
-      </div>
+      <GlobalContext>
+        <Header />
+        <div className={styles.separete}/>
+        <div className={styles.corpo}>
+          <Aside />
+          <Main />
+        </div>
+      </GlobalContext>
     </main>
   )
 }
